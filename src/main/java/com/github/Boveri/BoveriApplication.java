be202aea04f7631b50ccd9a8afd13686e64cd645
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-11 21:39:35 -0300

    first commit

---
+++ src/main/java/com/github/Boveri/BoveriApplication.java
@@ -0,0 +1,13 @@
+package com.github.Boveri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoveriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoveriApplication.class, args);
+	}
+
+}
